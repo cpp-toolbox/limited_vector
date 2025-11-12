ae78a85009f27e0fd02daea7e8bb392196ac86a8
ccn <ccn@cuppajoeman.com>
2025-11-12 00:22:01 -0500

    refactor: remove tpp file

--- limited_vector.hpp
+++ limited_vector.hpp
@@ -2,6 +2,7 @@
 #define LIMITED_VECTOR_HPP
 
 #include <vector>
+#include <stdexcept>
 
 /**
  * @brief A vector-like container that maintains a maximum size.
@@ -16,7 +17,7 @@ template <typename T> class LimitedVector {
      *
      * @param max_size The maximum number of elements the vector can hold.
      */
-    explicit LimitedVector(size_t max_size);
+    explicit LimitedVector(size_t max_size) : max_size_(max_size) {}
 
     /**
      * @brief Adds an element to the vector. If the vector exceeds the maximum size,
@@ -24,7 +25,12 @@ template <typename T> class LimitedVector {
      *
      * @param value The element to be added.
      */
-    void push_back(const T &value);
+    void push_back(const T &value) {
+        if (data_.size() >= max_size_) {
+            data_.erase(data_.begin());
+        }
+        data_.push_back(value);
+    }
 
     /**
      * @brief Access an element by index.
@@ -32,7 +38,7 @@ template <typename T> class LimitedVector {
      * @param index The index of the element to access.
      * @return The element at the specified index.
      */
-    T &operator[](size_t index);
+    T &operator[](size_t index) { return data_[index]; }
 
     /**
      * @brief Access an element by index (const version).
@@ -40,88 +46,102 @@ template <typename T> class LimitedVector {
      * @param index The index of the element to access.
      * @return The element at the specified index.
      */
-    const T &operator[](size_t index) const;
+    const T &operator[](size_t index) const { return data_[index]; }
 
     /**
      * @brief Gets the first element in the vector.
      *
      * @return A reference to the first element.
      */
-    T &front();
+    T &front() {
+        if (empty())
+            throw std::out_of_range("Attempted to access front of empty vector.");
+        return data_.front();
+    }
 
     /**
      * @brief Gets the first element in the vector (const version).
      *
      * @return A const reference to the first element.
      */
-    const T &front() const;
+    const T &front() const {
+        if (empty())
+            throw std::out_of_range("Attempted to access front of empty vector.");
+        return data_.front();
+    }
 
     /**
      * @brief Gets the last element in the vector.
      *
      * @return A reference to the last element.
      */
-    T &back();
+    T &back() {
+        if (empty())
+            throw std::out_of_range("Attempted to access back of empty vector.");
+        return data_.back();
+    }
 
     /**
      * @brief Gets the last element in the vector (const version).
      *
      * @return A const reference to the last element.
      */
-    const T &back() const;
+    const T &back() const {
+        if (empty())
+            throw std::out_of_range("Attempted to access back of empty vector.");
+        return data_.back();
+    }
 
     /**
      * @brief Gets the current size of the vector.
      *
      * @return The number of elements in the vector.
      */
-    size_t size() const;
+    size_t size() const { return data_.size(); }
 
     /**
      * @brief Checks if the vector is empty.
      *
      * @return True if the vector is empty, false otherwise.
      */
-    bool empty() const;
+    bool empty() const { return data_.empty(); }
 
     /**
      * @brief Clears all elements in the vector.
      */
-    void clear();
+    void clear() { data_.clear(); }
 
     /**
      * @brief Returns an iterator to the beginning of the vector.
      *
      * @return Iterator pointing to the first element.
      */
-    typename std::vector<T>::iterator begin();
+    typename std::vector<T>::iterator begin() { return data_.begin(); }
 
     /**
      * @brief Returns a const iterator to the beginning of the vector.
      *
      * @return Const iterator pointing to the first element.
      */
-    typename std::vector<T>::const_iterator begin() const;
+    typename std::vector<T>::const_iterator begin() const { return data_.begin(); }
 
     /**
      * @brief Returns an iterator to the end of the vector.
      *
      * @return Iterator pointing to the position after the last element.
      */
-    typename std::vector<T>::iterator end();
+    typename std::vector<T>::iterator end() { return data_.end(); }
 
     /**
      * @brief Returns a const iterator to the end of the vector.
      *
      * @return Const iterator pointing to the position after the last element.
      */
-    typename std::vector<T>::const_iterator end() const;
+    typename std::vector<T>::const_iterator end() const { return data_.end(); }
 
   private:
     size_t max_size_;     ///< The maximum number of elements the vector can hold.
     std::vector<T> data_; ///< The underlying storage for the vector elements.
 };
-
-#include "limited_vector.tpp"
 
 #endif // LIMITED_VECTOR_HPP
